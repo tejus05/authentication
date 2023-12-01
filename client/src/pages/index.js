fb9b2336fa0734db29cd8dc203ecc4655e2c9243
tejus <thequantumcoder@gmail.com>
2023-12-01 17:13:59 +0530

    feature/added routes and created pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,13 @@
+import About from "./About";
+import Home from "./Home";
+import Login from "./Login";
+import Profile from "./Profile";
+import Signup from "./Signup";
+
+export {
+  Home,
+  About,
+  Login,
+  Profile,
+  Signup
+}
