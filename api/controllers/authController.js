bc45a4473c0e0991c2e4d2380ad717cbefa97e40
tejus <thequantumcoder@gmail.com>
2023-12-02 10:45:45 +0530

    feature/create signup api route

---
+++ api/controllers/authController.js
@@ -0,0 +1,15 @@
+import User from "../models/userModel.js";
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req,res) => {
+  const { userName, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({userName,email,password: hashedPassword});
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "user created successfully" });
+  }
+  catch (err) {
+    res.status(500).json(err.message);
+  }
+}
--- api/index.js
+++ api/index.js
@@ -2,17 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouters from './routes/userRoute.js'
+import authRouters from './routes/authRoute.js';
 
+const app = express();
+
+app.use(express.json());
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
   .then(()=>{console.log("Connected to MonogDB")})
   .catch((err)=>{console.log(err)});
 
-const app = express();
 
 app.listen(3000, () => {
   console.log('Server listening on 3000')
 });
 
 app.use("/api/user", userRouters);
+
+app.use("/api/auth", authRouters);
