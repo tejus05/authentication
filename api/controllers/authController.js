ef37b54b654e2e6516aa7a5d313e18a3f6bc142a
tejus <thequantumcoder@gmail.com>
2023-12-02 14:33:09 +0530

    feature/error handling using middleware

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req,res) => {
+export const signup = async (req,res,next) => {
   const { userName, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({userName,email,password: hashedPassword});
@@ -10,6 +10,6 @@ export const signup = async (req,res) => {
     res.status(201).json({ message: "user created successfully" });
   }
   catch (err) {
-    res.status(500).json(err.message);
+    next(err);
   }
 }
